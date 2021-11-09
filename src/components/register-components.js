c28d42be84025942560706541c1737e63a38fb12
Stackbit Code Editor <projects@stackbit.com>
2021-11-09 12:01:52 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -1,12 +1,12 @@
 import dynamic from 'next/dynamic';
 import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
-
+import Featured from "./Featured.tsx"
 registerComponents({
     // Register all Stackbit components
     ...componentsMap,
 
-    FeaturedPostsSection: require("./Featured.tsx")
+    FeaturedPostsSection: 
     // Override any static or dynamic component,
     // or register your own dynamic component.
 });
