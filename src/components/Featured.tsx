06d55d4823ea0fcb19499bc6c1d45aa6178fad99
Stackbit Code Editor <projects@stackbit.com>
2021-11-09 12:00:53 +0000

    Featured.tsx: changed

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import Markdown from 'markdown-to-jsx';
 import { mapStylesToClassNames as mapStyles } from '../../utils/map-styles-to-class-names';
-import { getComponent } from '@stackbit/components/components-registry';
+import { getComponent } from '@stackbit/components/dist/components-registry';
 import ImageBlock from '@stackbit/components/dist/ImageBlock';
 
 export default function FeaturedPeopleSection(props) {
